af06b0c7fde7f77d6f8432789973942881ae9bc3
Dave Allen <dxa59@case.edu>
2014-05-10 20:37:24 -0400

    More controller stuff for PS3

--- New Unity Project/Assets/DTA/Ps3ControllerTest.cs
+++ New Unity Project/Assets/DTA/Ps3ControllerTest.cs
@@ -14,6 +14,13 @@ public class Ps3ControllerTest : MonoBehaviour {
 				Debug.Log(Input.inputString);
 		}
 
+
+/*
+foreach(string n in Input.GetJoystickNames()){
+Debug.Log("JOYSTICK NAMES: "+n);
+}
+*/
+
 		if(Input.GetAxisRaw("Horizontal") != 0){ // From name in Edit > Project Settings > Input
 			Debug.Log("D Pad Left/Right:" + Input.GetAxisRaw("Horizontal"));
 			//*DTA NOTE THAT THIS ISNT PER CONTROLLER
@@ -22,69 +29,106 @@ public class Ps3ControllerTest : MonoBehaviour {
 			Debug.Log("D Pad Up/Down:" + Input.GetAxisRaw("Vertical"));
 			//*DTA NOTE THAT THIS ISNT PER CONTROLLER
 		}
-		if (Input.GetKey("joystick 1 button 0")) {
+		if (Input.GetKey(KeyCode.Joystick1Button0)) {
 			Debug.Log("^");
 		}
-		if (Input.GetKey("joystick 1 button 1")) {
+		if (Input.GetKey(KeyCode.Joystick1Button1)) {
 			Debug.Log("O");
 		}
-		if (Input.GetKey("joystick 1 button 2")) {
+		if (Input.GetKey(KeyCode.Joystick1Button2)) {
 			Debug.Log("X");
 		}
-		if (Input.GetKey("joystick 1 button 3")) {
+		if (Input.GetKey(KeyCode.Joystick1Button3)) {
 			Debug.Log("[]");
 		}
-		if (Input.GetKey("joystick 1 button 4")) {
+		if (Input.GetKey(KeyCode.Joystick1Button4)) {
 			Debug.Log("L2");
 		}
-		if (Input.GetKey("joystick 1 button 5")) {
+		if (Input.GetKey(KeyCode.Joystick1Button5)) {
 			Debug.Log("R2");
 		}
-		if (Input.GetKey("joystick 1 button 6")) {
+		if (Input.GetKey(KeyCode.Joystick1Button6)) {
 			Debug.Log("L1");
 		}
-		if (Input.GetKey("joystick 1 button 7")) {
+		if (Input.GetKey(KeyCode.Joystick1Button7)) {
 			Debug.Log("R1");
 		}
-		if (Input.GetKey("joystick 1 button 8")) {
+		if (Input.GetKey(KeyCode.Joystick1Button8)) {
 			Debug.Log("SELECT");
 		}
-		if (Input.GetKey("joystick 1 button 9")) {
+		if (Input.GetKey(KeyCode.Joystick1Button9)) {
 			Debug.Log("START");
 		}
-		if (Input.GetKey("joystick 1 button 10")) {
+		if (Input.GetKey(KeyCode.Joystick1Button10)) {
 			Debug.Log("L3");
 		}
-		if (Input.GetKey("joystick 1 button 11")) {
+		if (Input.GetKey(KeyCode.Joystick1Button11)) {
 			Debug.Log("R3");
 		}
-		if (Input.GetKey("joystick 1 button 12")) {
+		if (Input.GetKey(KeyCode.Joystick1Button12)) {
 			Debug.Log("Supposedly the home button...");
-		}
-		if (Input.GetKey("joystick 1 button 13")) {
-			Debug.Log("13");
-		}
-		if (Input.GetKey("joystick 1 button 14")) {
-			Debug.Log("14");
-		}
-		if (Input.GetKey("joystick 1 button 15")) {
-			Debug.Log("15");
-		}
-		if (Input.GetKey("joystick 1 button 16")) {
-			Debug.Log("16");
-		}
-		if (Input.GetKey("joystick 1 button 17")) {
-			Debug.Log("17");
-		}
-		if (Input.GetKey("joystick 1 button 18")) {
-			Debug.Log("18");
-		}
-		if (Input.GetKey("joystick 1 button 19")) {
-			Debug.Log("19");
 		}
 //		if (Input.GetKey("joystick 2 button 0")) {
 //			Debug.Log("joystick 2 button 0");
 //		}	
-	
+		getAxis();
+	}
+
+
+
+
+	void getAxis()
+	{
+		string currentAxis="";
+		float axisInput=0;
+		if(Input.GetAxisRaw("X axis")> 0.3|| Input.GetAxisRaw("X axis") < -0.3)
+		{
+			currentAxis = "X axis";
+			axisInput = Input.GetAxisRaw("X axis");
+		}
+		
+		if(Input.GetAxisRaw("Y axis")> 0.3|| Input.GetAxisRaw("Y axis") < -0.3)
+		{
+			currentAxis = "Y axis";
+			axisInput = Input.GetAxisRaw("Y axis");
+		}
+		
+		if(Input.GetAxisRaw("3rd axis")> 0.3|| Input.GetAxisRaw("3rd axis") < -0.3)
+		{
+			currentAxis = "3rd axis";
+			axisInput = Input.GetAxisRaw("3rd axis");
+		}
+		
+		if(Input.GetAxisRaw("4th axis")> 0.3|| Input.GetAxisRaw("4th axis") < -0.3)
+		{
+			currentAxis = "4th axis";
+			axisInput = Input.GetAxisRaw("4th axis");
+		}
+		
+		if(Input.GetAxisRaw("5th axis")> 0.3|| Input.GetAxisRaw("5th axis") < -0.3)
+		{
+			currentAxis = "5th axis";
+			axisInput = Input.GetAxisRaw("5th axis");
+		}
+		
+		if(Input.GetAxisRaw("6th axis")> 0.3|| Input.GetAxisRaw("6th axis") < -0.3)
+		{
+			currentAxis = "6th axis";
+			axisInput = Input.GetAxisRaw("6th axis");
+		}
+		
+		if(Input.GetAxisRaw("7th axis")> 0.3|| Input.GetAxisRaw("7th axis") < -0.3)
+		{
+			currentAxis = "7th axis";
+			axisInput = Input.GetAxisRaw("7th axis");
+		}
+		
+		if(Input.GetAxisRaw("8th axis") > 0.3|| Input.GetAxisRaw("8th axis") < -0.3)
+		{
+			currentAxis = "8th axis";
+			axisInput = Input.GetAxisRaw("8th axis");
+		}
+
+		Debug.Log("AXIS INPUT: "+axisInput);
 	}
 }
--- docs/PS3 Controller/README.md
+++ docs/PS3 Controller/README.md
@@ -11,5 +11,19 @@ MotioninJoy_060003_amd64_signed.zip
 
 motionjoy.local.0.6.0003a.Full.zip
 
+To make changes, use application `DS3 Tool`
+
+
+**IMPORTANT**
+
+In DS3 Tool, in Local tab, set controller to PS 3 controller. To ensure things are working, click `Game Controller Panel` then `Properties`. In the screen that pops up, hit a bunch of things on the controller. Make sure all analog sticks are registered. If they aren't, restart the computer with the controller plugged in and try again.
+
+See `MotionInJoy Settings.png` for how to set it up and `MotionInJoy game controller properties.png` for how to ensure the joysticks are working.
+
+You will have to update Unity's input manager with more inputs to make this work. Check out the download here: http://wiki.etc.cmu.edu/unity3d/index.php/Joystick/Controller  --- note that 7th and 8th axis are the gyroscopes in the controller, and they overwrite the analog joysticks. Disable the 7th and 8th axis to see the others work. Also, as a test, create your own unity project for PS3 controllers that lists EVERY button and axis pressed - not just one of them.
+
+**/IMPORTANT**
 
 USING CONTROLLER: http://angryant.com/2013/08/04/Unity-Hacks-Dual-sticks/
+
+A lot of the issues seem to come from the DS3 driver utility. Playstation 1 setting is dpad without joysticks.
